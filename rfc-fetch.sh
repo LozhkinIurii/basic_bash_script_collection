bd9ce1dd5f9053c133e26ae8c26b85f798fb6ebe
Iurii_Lozhkin <lozhkinyulo@gmail.com>
2023-03-25 11:59:36 +0200

    file number and date added to output

--- rfc-fetch.sh
+++ rfc-fetch.sh
@@ -4,17 +4,21 @@
 # Fetch N random (N from command line argument)  RFC files, print out the title of the RFCs and number of lines in the RFC.
 # (https://www.ietf.org/rfc/rfc1237.txt)
 
+# Check if there is no arguments supplied to the script.
 if [ $# -eq 0 ]
 then
+# If no args, one rfc file is fetched.
     amount=1
 else
+# Only first argument is taken into account
     amount=$1
 fi
 rfc=1
+# Fetch a title from random rfc files
 while ! [ $rfc -gt $amount ]
 do
     rfc_number=$(($RANDOM % 9411 + 1))
-    curl -s "https://www.ietf.org/rfc/rfc$rfc_number.txt" | egrep -m 1 "^ *RFC $rfc_number.*[A-Z]{1}[a-z]* [0-9]{4}$" | sed "s/RFC $rfc_number *//" | sed "s/ *[A-Z]\{1\}[a-z]* [0-9]\{4\}$//"
+    curl -s "https://www.ietf.org/rfc/rfc$rfc_number.txt" | egrep -m 1 "^ *RFC $rfc_number"
     echo "Number of lines in the file is" $(curl -s "https://www.ietf.org/rfc/rfc$rfc_number.txt" | wc -l)
     echo
     ((rfc++))
